648cd47bd5071fb16f2a891c79fa9b7c21e6fe2a
ejossev <josef.sevcik@gmail.com>
2018-06-27 11:05:37 +0200

    read methods

--- contracts/TokenReclaim.sol
+++ contracts/TokenReclaim.sol
@@ -22,4 +22,27 @@ contract TokenReclaim{
         emit AccountRegister(msg.sender, name, pubKey);
     }
 
+    function account(address addr) constant public returns (string){
+        return _ethToSphtx[addr];
+    }
+
+    function keys(address addr) constant public returns (string){
+        return _accountToPubKey[_ethToSphtx[addr]];
+    }
+
+    function nameAvailable(string name) constant public returns (bool){
+        if( bytes(_accountToPubKey[name]).length != 0 )
+           return false;
+        if(bytes(name).length < 3 && bytes(name).length > 16)
+           return false;
+        bytes memory b = bytes(name);  
+        if( (b[0] < 'a' || b[0] > 'z') && ( b[0] < '0' || b[0] > '9' ) )
+           return false;
+        for(uint i=1;i< bytes(name).length; i++)
+           if( (b[0] < 'a' || b[0] > 'z') && ( b[0] < '0' || b[0] > '9' ) && b[i] != '-' && b[i] != '.' )
+              return false;
+        return true;   
+    }
+  
+
 }
